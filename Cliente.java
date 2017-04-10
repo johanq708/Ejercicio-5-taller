7845333b44cc4f2cf3ba8a4c445d91730687e195
johit <johit@DESKTOP-HQPQDH2>
2017-04-10 12:44:35 -0500

    ejercicio 5 categorias producto items

---
+++ Cliente.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio5;
+
+/**
+ *
+ * @author johit
+ */
+public class Cliente extends DomainObject{
+    private String nombre;
+    private String apellido;
+    private String telefono;
+    private String calle;
+    private String carrera;
+    private String ciudad;
+    private String zipcode;
+    private String pais;
+
+    public Cliente(String nombre, String apellido, String telefono, String calle, String carrera, String ciudad, String zipcode, String pais, String id) {
+        super(id);
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+        this.calle = calle;
+        this.carrera = carrera;
+        this.ciudad = ciudad;
+        this.zipcode = zipcode;
+        this.pais = pais;
+    }
+    
+}
